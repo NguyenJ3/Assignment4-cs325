6cd3505259dc554973e3743d42fba47ae7882ff1
Nguyen <jn@10-197-144-61.sds.oregonstate.edu>
2023-02-06 21:33:44 -0800

    Changed and highlighted bascase

--- MaxSet.py
+++ MaxSet.py
@@ -1,23 +1,32 @@
 def max_independent_set(nums):
-    n = len(nums) #n will be the lenght of nums 
-    if n == 0 or min(nums) < 0: 
-        return [] #stores elements from the if statement 
-    dp = [0] * n 
-    dp[0] = nums[0] #The first element that includes the max sum
-    dp[1] = max(nums[0], nums[1]) #first or second element 
-    
-    for i in range (2,n ): #loops the elements 
+    n = len(nums)
+    dp = [0] * n
+    selected = [False] * n
+
+    # handle the base case
+    if n == 0:
+        return []
+    if n == 1:
+        return [nums[0]] if nums[0] >= 0 else []
+
+    # calculate dp[i] as the max sum possible ending at index i
+    dp[0] = nums[0]
+    dp[1] = max(nums[0], nums[1])
+
+    for i in range(2, n):
+        # dp[i-1] represents the max sum without considering nums[i]
+        # dp[i-2] + nums[i] represents the max sum considering nums[i]
         dp[i] = max(dp[i-1], dp[i-2] + nums[i])
-    results =  [] #stores the list of elements within max 
-    i = n - 1 
 
-    #loop through the list reversing it 
+    # construct the selected list
+    i = n - 1
+    while i >= 0:
+        if dp[i] == dp[i-1]:
+            i -= 1
+        else:
+            selected[i] = True
+            i -= 2
 
-    while i >= 0: 
-        if i >= 2 and dp[i-2] + nums[i] >= dp[i-1]: 
-            results.append(nums[i]) # appends the element to the results 
-            i = i - 2
-        else: 
-            i = 1 - i
-
-    return results[:: -1 ] #returns the reverse list 
+    # return the selected numbers
+    result = [nums[i] for i in range(n) if selected[i]]
+    return result if result else []
